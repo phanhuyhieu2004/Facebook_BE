692bdc4719751f456290267efe41eff1d96549d7
phanhuyhieu2004 <hieuhuyphantn@gmail.com>
2024-05-07 10:17:12 +0700

    first-commit

---
+++ src/main/java/com/example/facebook_be/FacebookBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.facebook_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/facebook_be/FacebookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facebook_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
